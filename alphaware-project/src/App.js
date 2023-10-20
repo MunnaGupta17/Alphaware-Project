c42d117df5d72c3027e54f02e5b8d3d8e84827b9
MunnaGupta17 <mgabd17@gmail.com>
2023-10-20 11:50:38 +0530

    deployment configurations configured

--- alphaware-project/src/App.js
+++ alphaware-project/src/App.js
@@ -11,31 +11,20 @@ function App() {
   const [isCartVisible, setCartVisible] = useState(false);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    if (product.quantity > 0) {
+      // Quantity is greater than 0, allow addition to the cart
+      setCart([...cart, product]);
+    } else {
+      // Quantity is 0, display an alert
+      alert("Please select a quantity greater than 0.");
+    }
+    
   };
-
-  // const addToCart = (product) => {
-  //   const existingProductIndex = cart.findIndex((item) => item.id === product.id);
-  //   if (existingProductIndex !== -1) {
-  //     // Product already in cart, update quantity
-  //     const updatedCart = [...cart];
-  //     updatedCart[existingProductIndex].quantity += 1;
-  //     setCart(updatedCart);
-  //   } else {
-  //     // Product not in cart, add it
-  //     setCart([...cart, { ...product, quantity: 1 }]);
-  //   }
-  // };
 
   const removeFromCart = (product) => {
     const updatedCart = cart.filter((item) => item._id !== product._id);
     setCart(updatedCart);
   };
-  // const removeFromCart = (productId) => {
-  //   // Filter the cart to remove the product with a matching ID
-  //   const updatedCart = cart.filter((product) => product.id !== productId);
-  //   setCart(updatedCart);
-  // };
 
 
   return (
